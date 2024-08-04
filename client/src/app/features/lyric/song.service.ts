bde8254169b6971b50ed66fa05bfa620fc599f96
John Schellinger <schellingerj1@gmail.com>
2024-08-03 23:21:00 -0400

    Serverless v3

---
+++ client/src/app/features/lyric/song.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { ScrapedSong } from './song.interface';
+
+@Injectable()
+export class SongService {
+  private http = inject(HttpClient);
+
+  get random$(): Observable<ScrapedSong> {
+    const endpoint =
+      'https://0h296ci3w4.execute-api.us-east-1.amazonaws.com/random';
+
+    return this.http.get<ScrapedSong>(endpoint).pipe(
+      map((song) => {
+        const numLyrics = 4;
+        const rand = this.getRandomInt(
+          numLyrics,
+          (song.lyrics?.length || 4) - numLyrics
+        );
+        return {
+          ...song,
+          lyrics: song.lyrics?.slice(rand, rand + numLyrics) || [],
+        };
+      })
+    );
+  }
+
+  private getRandomInt(min: number, max: number) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  }
+}
